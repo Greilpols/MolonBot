ea009961919c3034f0c7218894c361a71b09241d
Daniel Skogli <daniel.skogli89@gmail.com>
2023-08-02 22:45:25 +0300

    updating to make username to string for database

--- databasing.py
+++ databasing.py
@@ -1,21 +1,21 @@
 from replit import db
 
 def addUser(user):
-  if user in db:
+  if str(user) in db:
     return "user in db"
   else:
     try:
-      db[user] = 0
+      db[str(user)] = 0
       return "user added to db"
     except Exception:
       return "user not in db, adding failed"
 
 def showScore(user):
-  print (user + "testforuser")
-  if user in db:
-    print (db[user])
+  print (str(user) + "testforuser")
+  if str(user) in db:
+    print (str(db[user]))
     return ("Your score is " + db[user])
   else:
     addUser(user)
-    return ("Username not found in database. You have now been added. Your score is " + db[user])
+    return ("Username not found in database. You have now been added. Your score is " + str(db[user]))
   return "Something went terribly wrong. Try again later. If problem persist, pester coder."

985d9b989d62319eb536e0e4399dcaabd98417df
Daniel Skogli <daniel.skogli89@gmail.com>
2023-08-12 23:10:02 +0300

    adding deathrolling starter

--- rolling.py
+++ rolling.py
@@ -13,8 +13,9 @@ def tryingRolling(roll):
     rolled = random.randrange(num)
     print(rolled)
   except Exception:
+    #if failed to get number, just /roll 100
     print("Failure to get int")
-    rolled = 'x'
+    rolled = random.randrange(100)
   finally:
     return rolled
 
@@ -22,4 +23,11 @@ def deathRolling(roll, userName):
   result = tryingRolling(roll)
   #call to database if loss
   #return comment
-  return "placeholder"
+  points = 100 #temp while deciding how to hold orig num... just use a list or summit? only want ganes to be temp after all
+  gameWinner = "placeholderman"
+  if result == 0:
+    databasing.reduce(userName, points)
+    databasing.add(gameWinner, points)
+    return ("Victory! " + gameWinner + " has bested " + userName + "! " + points + " points to " + gameWinner + "!")
+  return ("Let the games... be under way!")
+  # note to self - do we want to use something silly as molonbucks or gold or?

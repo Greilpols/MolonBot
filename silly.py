383b73de80a65aae8a72c6e50e07d5e6cd152988
Daniel Skogli <daniel.skogli89@gmail.com>
2023-08-13 23:38:04 +0300

    adding jokes to silly

--- silly.py
+++ silly.py
@@ -2,6 +2,12 @@ import random
 
 def silly(amusementType):
   if (amusementType == 'joke'):
-    return "joke"
+    try:
+      with open("silly.txt", "r") as f:
+        jokes = f.readlines()
+      return jokes[int(random.randrange(10))]
+    except Exception:
+      result = "Something went wrong. Please try again later."
+    return result
   else:
     return "mirror"

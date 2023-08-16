02e820fcbc1f45e9fb061d15d0359f3b5c01b8f4
Daniel Skogli <daniel.skogli89@gmail.com>
2023-08-16 22:29:48 +0300

    next question prompt

--- quiz.py
+++ quiz.py
@@ -20,12 +20,18 @@ def quiz(quizTypeToStart):
   return ("Now starting a quiz of the " + quizType + " variety!")
 
 def nextQuestion():
-    #for next question, so keep continuing on the same quiz
-    randomNumber = random.randrange(5)
+  #for next question, so keep continuing on the same quiz
+  global quizCounter
+  quizCounter += 1
+  randomNumber = random.randrange(5) #temp value for testing - WIP change number to num of q's available
+  global quizQuestionsAsked
+  quizQuestionsAsked.append(randomNumber)
+  if quizType == "Uninitialized":
+    return ["No quiz type started. write !quiz <type> to start one"]
 
-    warcraftResponse = [db[warcraft_Quiz][randomNumber], db[warcraft_QuizAnswers][randomNumber]]
+  warcraftResponse = [db[warcraft_Quiz][randomNumber], db[warcraft_QuizAnswers][randomNumber]]
 
-    return warcraftResponse
+  return warcraftResponse
 
 def warcraftQuiz():
     randomNumber = random.randrange(5)

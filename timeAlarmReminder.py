6f8d7cd37958d06e1a8a3a46f737d8aa3b3a2ca0
Daniel Skogli <daniel.skogli89@gmail.com>
2023-08-23 23:41:39 +0300

    testing some timings

--- timeAlarmReminder.py
+++ timeAlarmReminder.py
@@ -1,4 +1,12 @@
 #intended to be used for daily reminders, alarms as well as other time related things (i.e. temporarily silence someone)
 
-def botTiming(messageContent):
+import datetime
+import time
+
+
+def botTiming(messageContent): #'!timer' or '!reminder' or '!schedule'
+  print(datetime.datetime.now())
+  time.sleep(30) #test
+  #nah need a better way than this...
+  print(datetime.datetime.now())
   return "placeholder"

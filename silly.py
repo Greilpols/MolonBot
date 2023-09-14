36bfd6ea96987dbb8ae9d5fb054be60f0182d1e3
Daniel Skogli <daniel.skogli89@gmail.com>
2023-09-14 23:49:07 +0300

    starting to fix !joke calls

--- silly.py
+++ silly.py
@@ -5,7 +5,8 @@ def silly(amusementType):
     try:
       with open("silly.txt", "r") as f:
         jokes = f.readlines()
-      return jokes[int(random.randrange(10))]
+        joke = jokes[int(random.randrange(3))]
+      return joke
     except Exception:
       result = "Something went wrong. Please try again later."
     return result

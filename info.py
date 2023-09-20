35d204239ef9819c19b8a621366f36d5eecd9a26
Daniel Skogli <daniel.skogli89@gmail.com>
2023-09-20 23:38:37 +0300

    just text changes in !info and !help

--- info.py
+++ info.py
@@ -1,14 +1,8 @@
 #just a file for general info and misc responses
 
 def infoResponse(messageContent, author):
-  textBlock = "This is a placeholder for general info when using !info"
-  infoResponse = ("Hi! " + str(author) + " I am a simple bot for Molon. For a list of commands, type '!help'. For info about a specific command type !help <command>.")
-  return textBlock
-  #return infoResponse
+  return ("Hi! " + str(author) + " I am a simple bot for Molon. For a list of commands, type '!help'. For info about a specific command type !help <command>.")
 
 def helpResponse(messageContent):
-  listedResponses = ["test", "test2", "!help", "!info", "!roll <number>", "!deathroll <bet value>" "!suggestion", "!score", "!add", "!reduce"]
-  #listedResponses = "List of commands: !info, !help, !roll, !points, !suggestion, , "
-  textBlock = "Placeholder for a list of different commands that the bot should be capable of doing"
-#  return textBlock
+  return ("!info, !help, !roll <number>, !deathroll <bet value>, !suggestion, !score, !add, !reduce, !joke, !timer, !schedule (and more, it is WIP after all)")
   return listedResponses
